576216806c0ffabea7902bd6c1e90f907e0b47e6
pkyou <471162774@qq.com>
2019-09-30 16:55:08 +0800

    set 'X' to every square

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 export default class Square extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={stateValue:null}
+    }
+
     render(){
         return(
-            <button className="square">
-                {this.props.value}
+            <button className="square" 
+            onClick = {()=> this.setState({stateValue : 'X'})}>
+                {this.state.stateValue}
             </button>
         )
     }
